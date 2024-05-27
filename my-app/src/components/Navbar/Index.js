2fe8ae18af95a4a2efcff61da904750b466aceea
DanaeD1867 <116097166+DanaeD1867@users.noreply.github.com>
2024-05-27 13:25:02 -0700

    add navbar component to index file

---
+++ my-app/src/components/Navbar/Index.js
@@ -0,0 +1,45 @@
+import React from "react"; 
+import{
+    Nav, 
+    NavLink, 
+    Bars, 
+    NavMenu, 
+    NavBtn, 
+    NavBtnLink
+} from "./navbarelements"; 
+
+const Navbar = () => {
+
+    return(
+        <>
+            <Nav> 
+                <Bars />
+                <NavMenu> 
+                    <NavLink to="/bookings">
+                        Bookings
+                    </NavLink>
+
+                    <NavLink to="/gallery">
+                        Gallery
+                    </NavLink>
+
+                    <NavLink to="/merch">
+                        Merchandise
+                    </NavLink>
+
+                    <NavLink to="/murals">
+                        Murals
+                    </NavLink>
+
+                    <NavLink to="/contact">
+                        Contact Us
+                    </NavLink>
+
+                </NavMenu>
+            </Nav>
+        </>
+    ); 
+    
+}; 
+
+export default Navbar;
